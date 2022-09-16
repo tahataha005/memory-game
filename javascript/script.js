edef08cb4774f637f7ee0979947d09f1325b86d5
tahataha005 <taha.f.taha005@gmail.com>
2022-09-16 23:30:23 +0300

    Select 2 cards only

--- javascript/script.js
+++ javascript/script.js
@@ -3,14 +3,30 @@ window.onload = () => {
     const resetBtn = document.getElementById("reset")
     console.log(cards)
 
+    let clicked = 0
+
+    const reset = (cards) => {
+        cards.forEach(card => {
+            card.classList.remove("clicked")
+        });
+    }
 
     cards.forEach(card => {
         
         card.addEventListener("click", () => {
+            console.log(clicked)
+            if(clicked == 0 || clicked == 1){
+            clicked+=1
             card.classList.toggle("clicked")
-
-
+            
+            }
+            else if(clicked == 2){
+                clicked = 0
+                reset(cards)
+            }
+            
         })
+
     });
 
     resetBtn.addEventListener("click",() => {
@@ -18,6 +34,7 @@ window.onload = () => {
         cards.forEach(card => {
             card.classList.remove("clicked")
     })
+
 
 })
 }
